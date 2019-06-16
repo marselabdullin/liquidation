7189f168ae847ab2ea1a8f7d79e628878e9f543e
marselabdullin <abdmars@mail.ru>
2019-06-16 22:42:12 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(function(){
 
+    $('.slider__inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+    });
 
 
 
 
-    
+
+
 })
